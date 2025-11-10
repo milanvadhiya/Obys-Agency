047a961363fbf61b90f8f7f0cb3f5b514056209f
milan vadhiya <vadhiyamilan907@gmail.com>
2025-11-10 09:44:06 +0530

    page1 done!

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.3,
+  });
+
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 33);
+    },
+  });
+
+  tl.from("#last-heading ", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.2,
+    delay: -0.5,
+  });
+
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay:  0,
+  });
+
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1200,
+    duration:0.6,
+    opacity: 0,
+  });
+
+  tl.to("#loader", {
+    display: "none",
+  });
+    tl.from("#nav",{
+  opacity:0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+   y:120,
+   stagger:0.2,
+  });
+}
+function crsrAnimation(){
+  document.addEventListener("mousemove", function (dets) {
+  gsap.to("#crsr",{
+    left:dets.x,
+    top:dets.y,
+  })
 });
+Shery.makeMagnet("#nav-part2 h4");
+}
 
 
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
-        h5timer.innerHTML = grow++;
-      } else {
-        h5timer.innerHTML = 100;
-      }
-    }, 33);
-  },
-});
-tl.to(".line h2",{
-    animationName:"anime",
-    opacity:1
-})
-
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 3,
-});
-
-
-tl.from("#page1",{
-    delay:0.2,
-    y:1200,
-    opacity:0
-})
-
-
-tl.to("#loader", {
-  display:"none",
-});
+crsrAnimation();
+loadingAnimation();
